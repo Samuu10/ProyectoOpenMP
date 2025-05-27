ffe09424079fbbcdcd2e3969c9f1a9b6924b5388
Samuu10 <svallmun@myuax.com>
2025-05-27 10:49:45 +0200

    Integración de OpenMP y pequeña prueba

--- main.c
+++ main.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
+#include <omp.h>
 
-int main(void) {
-    printf("Hello, World!\n");
+int main() {
+#pragma omp parallel
+    {
+        int id = omp_get_thread_num();
+        printf("Hola desde el hilo %d\n", id);
+    }
     return 0;
 }
